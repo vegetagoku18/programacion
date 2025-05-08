0921672d83adbed95648a62af62d4043eb97678e
Carlos <carlos.paz.miranda@gmail.com>
2025-05-08 12:24:57 +0200

    Hacer que cambie los decimales

--- src/main/java/programacion/Application.java
+++ src/main/java/programacion/Application.java
@@ -7,7 +7,7 @@ public class Application {
   public static void main(String[] args) {
     Ejercicio14 ejercicio = new Ejercicio14();
     ejercicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    ejercicio.setSize(500, 500);
+    ejercicio.setSize(600, 600);
     ejercicio.setVisible(true);
   }
 }
--- src/main/java/programacion/Ejercicio14.java
+++ src/main/java/programacion/Ejercicio14.java
@@ -21,6 +21,11 @@ public class Ejercicio14 extends JFrame implements ActionListener, ItemListener 
     JRadioButton rbtProducto;
     JRadioButton rbtDivision;
     JButton btnCalcular;
+    JLabel lblInformarError;
+    JComboBox<Integer> cmbDecimales;
+    Timer tmr;
+    int contadorSegundos;
+    int contadorMinutos;
 
     public Ejercicio14() {
         super("Calculadora");
@@ -46,22 +51,22 @@ public class Ejercicio14 extends JFrame implements ActionListener, ItemListener 
 
         rbtSuma = new JRadioButton("+");
         rbtSuma.setSelected(true);
-        rbtSuma.setBounds(20, 50, 60, 60);
+        rbtSuma.setBounds(20, 50, 60, 40);
         rbtSuma.addItemListener(this);
         add(rbtSuma);
 
         rbtResta = new JRadioButton("-");
-        rbtResta.setBounds(80, 50, 60, 60);
+        rbtResta.setBounds(80, 50, 60, 40);
         rbtResta.addItemListener(this);
         add(rbtResta);
 
         rbtProducto = new JRadioButton("*");
-        rbtProducto.setBounds(150, 50, 60, 60);
+        rbtProducto.setBounds(150, 50, 60, 40);
         rbtProducto.addItemListener(this);
         add(rbtProducto);
 
         rbtDivision = new JRadioButton("/");
-        rbtDivision.setBounds(220, 50, 60, 60);
+        rbtDivision.setBounds(220, 50, 60, 40);
         rbtDivision.addItemListener(this);
         add(rbtDivision);
 
@@ -71,17 +76,52 @@ public class Ejercicio14 extends JFrame implements ActionListener, ItemListener 
         grupoOperadores.add(rbtProducto);
         grupoOperadores.add(rbtDivision);
 
-        btnCalcular = new JButton("Calcular");
-        btnCalcular.setBounds(280, 70, 100, 20);
+        btnCalcular = new JButton("Operación");
+        btnCalcular.setBounds(20, 100, 100, 20);
         btnCalcular.addActionListener(this);
         add(btnCalcular);
+
+        lblInformarError = new JLabel("");
+        lblInformarError.setBounds(130, 100, 250, 20);
+        add(lblInformarError);
+
+        cmbDecimales = new JComboBox<>();
+        cmbDecimales.setBounds(20, 130, 80, 30);
+        for (int i = 0; i <= 5; i++) {
+            cmbDecimales.addItem(i);
+        }
+        cmbDecimales.setSelectedItem(0);
+        add(cmbDecimales);
+
+        contadorSegundos = 0;
+        contadorMinutos = 0;
+        tmr = new Timer(1000, this);
+        tmr.start();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        float resultado = 0;
-        resultado = Float.parseFloat(txfNumeroUno.getText().trim()) + Float.parseFloat(txfNumeroDos.getText().trim());
-        lblIgual.setText(String.format("= %f0.2", resultado));
+        if (e.getSource() == tmr) {
+            contadorSegundos++;
+            if (contadorSegundos >= 60) {
+                contadorSegundos = 0;
+                contadorMinutos++;
+            }
+            this.setTitle(String.format("%2d:%2d", contadorMinutos, contadorSegundos));
+        } else {
+            try {
+
+                float resultado = 0;
+                resultado = Float.parseFloat(txfNumeroUno.getText().trim())
+                        + Float.parseFloat(txfNumeroDos.getText().trim());
+                lblIgual.setText(String.format("= %f0.2", resultado));
+                lblInformarError.setText("");
+
+            } catch (NumberFormatException nfe) {
+                lblInformarError.setText("Debe rellenar todos los parámetros");
+                lblInformarError.setForeground(Color.RED);
+            }
+        }
     }
 
     @Override
@@ -95,9 +135,15 @@ public class Ejercicio14 extends JFrame implements ActionListener, ItemListener 
                 if (e.getSource() == rbtProducto) {
                     lblOperacion.setText("*");
                 } else {
-                    lblOperacion.setText("/");
+                    if (e.getSource() == rbtDivision) {
+                        lblOperacion.setText("/");
+                    } else {
+
+                    }
                 }
             }
         }
+        lblIgual.setText("=");
+
     }
 }
